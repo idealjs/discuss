f2ece3f40b3535c6ed672973749fb704fcf27224
cqh963852 <cqh963852@hotmail.com>
2021-09-25 22:01:56 +0800

    update: update profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,17 @@
-import { signIn, signOut,useSession } from "next-auth/client";
-import { Fragment } from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { Fragment, useCallback } from "react";
 
 export default function Component() {
   const [session, loading] = useSession();
+  const onClick = useCallback(async () => {
+    await fetch("api/users");
+  }, []);
   if (session) {
     return (
       <Fragment>
         Signed in as {session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <button onClick={onClick}>req</button>
       </Fragment>
     );
   }
