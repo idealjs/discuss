9787088002fd276c3003525899101d574e8ec07a
cqh963852 <cqh963852@hotmail.com>
2021-09-27 00:46:11 +0800

    update: update env variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -28,14 +28,14 @@ export default NextAuth({
     }),
     Providers.Email({
       server: {
-        host: "",
+        host: process.env.EMAIL_HOST,
         port: 25,
         auth: {
-          user: "",
-          pass: "",
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
         },
       },
-      from: "",
+      from: process.env.EMAIL_USERNAME,
     }),
   ],
   jwt: {
