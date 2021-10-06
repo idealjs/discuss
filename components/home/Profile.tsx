6bfdc313465210420989274307b294d8e886dd34
cqh963852 <cqh963852@hotmail.com>
2021-10-06 22:14:13 +0800

    chore: move file

---
+++ components/home/Profile.tsx
@@ -0,0 +1,25 @@
+import { Session } from "next-auth";
+import { signOut } from "next-auth/client";
+import { useCallback } from "react";
+
+interface IProps {
+  session: Session;
+}
+
+const Profile = (props: IProps) => {
+  const { session } = props;
+
+  const onClick = useCallback(async () => {
+    await fetch("api/users");
+  }, []);
+
+  return (
+    <div>
+      <div>Signed in as {session.user?.name}</div>
+      <button onClick={() => signOut()}>Sign out</button>
+      <button onClick={onClick}>req</button>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ pages/home/index.tsx
@@ -0,0 +1,20 @@
+import { signIn, useSession } from "next-auth/client";
+
+import Intro from "../../components/home/Intro";
+import Profile from "../../components/home/Profile";
+
+const Home = () => {
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return null;
+  }
+
+  if (session) {
+    return <Profile session={session} />;
+  }
+
+  return <Intro />;
+};
+
+export default Home;
